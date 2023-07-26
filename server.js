34af9a30c2a5b2f3beec37b33ef8e068cfe01811
theacademydvr <theacademy.dvr@gmail.com>
2023-07-25 22:38:03 -0500

    first commit

---
+++ server.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const app = express();
+const http = require('http');
+const server = http.createServer(app);
+const logger = require('morgan');
+const cors = require('cors');
+
+const port = process.env.PORT || 3000;
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+app.use(cors());
+
+app.disable('x-powered-by');
+
+app.set('port', port);
+
+server.listen(3000, '192.168.1.17' || 'localhost', function(){
+    console.log('Ejecutando la aplicacion '+process.pid+' Iniciado en el puerto '+port)
+});
+
+
+app.get('/', (req, res)=>{
+    res.send('Ruta raiz del backend')
+});
+app.get('/test', (req, res)=>{
+    res.send('Es un test')
+});
+
+app.use((err, req, res, next)=>{
+    console.log(err);
+    res.status(err.status || 500).send(err.stack)
+});
