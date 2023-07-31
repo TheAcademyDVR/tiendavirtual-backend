2cbc8a497564cf7f9f9f806e57959e4c32ff46b3
theacademydvr <theacademy.dvr@gmail.com>
2023-07-30 22:35:45 -0500

    Validacion de usuarios

--- model/user.js
+++ model/user.js
@@ -1,8 +1,12 @@
 const connection = require('../config/connection');
+const  bcrypt = require('bcryptjs');
 
 const User = {};
 
-User.create = (user, result) => {
+User.create = async (user, result) => {
+
+    const hashClave = await bcrypt.hash(user.clave, 10);
+
     const sql = `
         INSERT INTO 
             usuarios(nombre, apellido, email, telefono, imagen, clave, creado, modificado)
@@ -10,7 +14,7 @@ User.create = (user, result) => {
     `;
     connection.query(
         sql,
-        [ user.nombre, user.apellido, user.email, user.telefono, user.imagen,  user.clave, new Date(), new Date() ],
+        [ user.nombre, user.apellido, user.email, user.telefono, user.imagen,  hashClave, new Date(), new Date() ],
         (err, res) =>{
             if(err){
                 console.log('Error en usuarios es: ',err);
